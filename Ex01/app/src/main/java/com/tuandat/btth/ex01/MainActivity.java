c2aad1aadc90a906891d63eabd541787e453f0ab
Tuan Dat <datto25082003@gmail.com>
2024-09-25 01:42:34 +0700

    Complete Ex01

--- Ex01/app/src/main/java/com/tuandat/btth/ex01/MainActivity.java
+++ Ex01/app/src/main/java/com/tuandat/btth/ex01/MainActivity.java
@@ -19,13 +19,13 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-//        edt_A = findViewById(R.id.edt_A);
-//        edt_B = findViewById(R.id.edt_B);
-//        edt_Result = findViewById(R.id.edt_Result);
-//        btn_Sum = findViewById(R.id.btn_Sum);
-//        btn_Sum.setOnClickListener(v -> {
-//            caculateSum();
-//        });
+        edt_A = findViewById(R.id.edt_A);
+        edt_B = findViewById(R.id.edt_B);
+        edt_Result = findViewById(R.id.edt_Result);
+        btn_Sum = findViewById(R.id.btn_Sum);
+        btn_Sum.setOnClickListener(v -> {
+            caculateSum();
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -34,24 +34,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-//    private void caculateSum() {
-//        String str_Number_A = edt_A.getText().toString();
-//        String str_Number_B = edt_B.getText().toString();
-//
-//        if(str_Number_A.isEmpty() || str_Number_B.isEmpty()) {
-//            edt_Result.setText("Please enter two numbers");
-//            return;
-//        }
-//
-//        try {
-//            int number_A = Integer.parseInt(str_Number_A);
-//            int number_B = Integer.parseInt(str_Number_B);
-//
-//            int result = number_A + number_B;
-//            edt_Result.setText(String.valueOf(result));
-//        }catch (NumberFormatException e){
-//            edt_Result.setText("Please enter number");
-//        }
-//
-//    }
+    private void caculateSum() {
+        String str_Number_A = edt_A.getText().toString();
+        String str_Number_B = edt_B.getText().toString();
+
+        if(str_Number_A.isEmpty() || str_Number_B.isEmpty()) {
+            edt_Result.setText("Please enter two numbers");
+            return;
+        }
+
+        try {
+            int number_A = Integer.parseInt(str_Number_A);
+            int number_B = Integer.parseInt(str_Number_B);
+
+            int result = number_A + number_B;
+            edt_Result.setText(String.valueOf(result));
+        }catch (NumberFormatException e){
+            edt_Result.setText("Please enter number");
+        }
+
+    }
 }
