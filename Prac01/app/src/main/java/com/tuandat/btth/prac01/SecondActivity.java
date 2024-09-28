4277257714b705c69b2c147bda1edd045bd9d8b4
Tuan Dat <datto25082003@gmail.com>
2024-09-28 16:41:53 +0700

    Create layout second activity and edit layout first activity

--- Prac01/app/src/main/java/com/tuandat/btth/prac01/SecondActivity.java
+++ Prac01/app/src/main/java/com/tuandat/btth/prac01/SecondActivity.java
@@ -2,8 +2,6 @@ package com.tuandat.btth.prac01;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +9,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        Button btnGoToSecondActivity = findViewById(R.id.btnGoToSecondActivity);
-        btnGoToSecondActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Tạo Intent để chuyển sang màn hình thứ 2
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_second);
+        // Hiển thị nút "Up" trên Action Bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    // Xử lý sự kiện khi nhấn nút "Up"
+    @Override
+    public boolean onSupportNavigateUp() {
+        // Quay lại MainActivity
+        Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();  // Kết thúc SecondActivity để ngăn nó tồn tại trong back stack
+        return true;
     }
 }
