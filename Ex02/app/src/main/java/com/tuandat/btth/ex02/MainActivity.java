397d28997abde7488ae08f6691261c07bd3d3ac9
Tuan Dat <datto25082003@gmail.com>
2024-10-01 17:55:04 +0700

    Complete Ex02

--- Ex02/app/src/main/java/com/tuandat/btth/ex02/MainActivity.java
+++ Ex02/app/src/main/java/com/tuandat/btth/ex02/MainActivity.java
@@ -1,6 +1,9 @@
 package com.tuandat.btth.ex02;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,60 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnCallDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"63KTPM2 - onCreate()",Toast.LENGTH_SHORT).show();
+
+        btnCallDialog = findViewById(R.id.btnCallDialog);
+        btnCallDialog.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SubActivity.class);
+            startActivity(intent);
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"63KTPM2 - onDestroy()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"63KTPM2 - onPause()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"63KTPM2 - onRestart()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"63KTPM2 - onResume()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"63KTPM2 - onStart()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"63KTPM2 - onStop()",Toast.LENGTH_SHORT).show();
+    }
+
 }
--- Ex02/app/src/main/java/com/tuandat/btth/ex02/SubActivity.java
+++ Ex02/app/src/main/java/com/tuandat/btth/ex02/SubActivity.java
@@ -1,6 +1,8 @@
 package com.tuandat.btth.ex02;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SubActivity extends AppCompatActivity {
-
+    Button btnOk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sub);
+
+        btnOk = findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(v -> finish());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+
 }
